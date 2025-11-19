473fd0e3c7772d2948b2a24b168da05cff3be149
Tristan Trunez <tristan.trunez@outlook.de>
2025-11-19 18:36:19 +0100

    Frontend Testfunktion

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,14 @@
+// src/router/index.ts
 import { createRouter, createWebHistory } from 'vue-router'
+import RoomsView from '../views/RoomsView.vue'
+
+const routes = [
+  { path: '/rooms', name: 'Rooms', component: RoomsView }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes
 })
 
 export default router
